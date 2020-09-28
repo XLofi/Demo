4831d1549ea88ac675e53378c8515747c850a22f
XLofi <eb891048@ens.uqam.ca>
2020-09-28 14:26:12 -0400

    Fizzbuzz

--- Fizzbuzz/src/FizzBuzz.java
+++ Fizzbuzz/src/FizzBuzz.java
@@ -4,7 +4,7 @@ import java.util.stream.IntStream;
 
 public class FizzBuzz {
     public static void main(String[] args) throws Exception {
-        fizzBuzzInJava8(100);
+        fizzBuzzInJava8(200);
     }
 
     private static void fizzBuzzInJava8(int num) {
