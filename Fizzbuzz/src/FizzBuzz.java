2d4c841746cf742081482fadcd8f14d9faf3ce38
XLofi <eb891048@ens.uqam.ca>
2020-09-28 14:24:00 -0400

    Fizzbuzz

---
+++ Fizzbuzz/src/FizzBuzz.java
@@ -0,0 +1,15 @@
+package Fizzbuzz.src;
+
+import java.util.stream.IntStream;
+
+public class FizzBuzz {
+    public static void main(String[] args) throws Exception {
+        fizzBuzzInJava8(100);
+    }
+
+    private static void fizzBuzzInJava8(int num) {
+        IntStream.rangeClosed(1, 100)
+                .mapToObj(i -> i % 5 == 0 ? (i % 7 == 0 ? "FizzBuzz" : "Fizz") : (i % 7 == 0 ? "Buzz" : i))
+                .forEach(System.out::println);
+    }
+}
